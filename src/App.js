106bab39abb5be4654fec0e16dddbc00542c67cd
Manu George <manu@trackplex.com>
2020-11-12 03:03:33 +0530

    Made some changes to sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="-home">
       <Header/>
+      <div className="app__body">
+       <Sidebar/>
+
+      </div>
     </div>
   );
 }
