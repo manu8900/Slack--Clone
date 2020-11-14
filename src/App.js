c168c8e0741dcddaa5a686653165eea11ffefb12
Manu George <manu@trackplex.com>
2020-11-14 16:19:52 +0530

    Styled Chat component & added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
-
+import Chat from './Components/Chat';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="-home">
+    <div className="app">
+      <Router>
       <Header/>
       <div className="app__body">
        <Sidebar/>
-
+       <Switch>
+         <Route path="/room/:roomId" component={Chat}/>
+       </Switch>
       </div>
+      </Router>
     </div>
   );
 }
